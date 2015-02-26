c05e34d5dc025a5ac038914fdbdcbf20d71676ee
Robert van der Linde <r.vanderlinde@pharma-intelligence.nl>
2015-02-26 22:47:48 +0100

    Updated GS1-128 support
    Signed-off-by: Robert van der Linde <r.vanderlinde@pharma-intelligence.nl>

--- Service/Barcode/Barcode.php
+++ Service/Barcode/Barcode.php
@@ -7,31 +7,32 @@ class Barcode
     
     protected $type;
     
+    protected $attributes = array();
+    
     public function __construct($barcode) {
         $this->barcode = $barcode;    
         $this->type = new BarcodeType($barcode);
+        if($this->type->isGtinExpanded() || $this->type->isGtin14()) {
+            $parser = new GS1128BarcodeParser();
+            $this->attributes = $parser->parse($barcode); 
+        } else {
+            $this->attributes['productIdentifier'] = $this->barcode;
+        }
     }
     
+    public function getBarcode() {
+        if(array_key_exists('batch', $this->attributes))
+            return $this->attributes['batch'];
+        return null;
+    }
     public function getProductIdentifier() {
-        if($this->type->isGtinExpanded() || $this->type->isGtin14()) {
-            return substr($this->barcode, 3, 13);
-        }
-        return $this->barcode;
+        return $this->attributes['productIdentifier'];
     }
     
     public function getVervalDatum() {
-        if(!$this->type->isGtinExpanded()) {
-            return \DateTimeImmutable::createFromFormat('Y-m-d', '0000-00-00');
-        }
-        
-        $vervalDatumRegexp = '/17(\d{6})/';
-        if(preg_match($vervalDatumRegexp, substr($this->barcode, 14)) === 0) {
-            return \DateTime::createFromFormat('Y-m-d', '0000-00-00');
-        }
-        preg_match($vervalDatumRegexp, substr($this->barcode, 14), $matches);
-        if(substr($matches[1], 4) == '00')
-            $matches[1] = substr($matches[1], 0, 4).'01';
-        return \DateTime::createFromFormat('ymd', $matches[1]);
+        if(array_key_exists('expirationDate', $this->attributes))
+            return $this->attributes['expirationDate'];
+        return \DateTime::createFromFormat('Y-m-d', '0000-00-00');
     }
     
     /**
---
+++ Service/Barcode/GS1128BarcodeParser.php
@@ -0,0 +1,80 @@
+<?php
+namespace PharmaIntelligence\GstandaardBundle\Service\Barcode;
+
+class GS1128BarcodeParser
+{
+    protected $mapping = array(
+        '01' => array(
+            'name' => 'productIdentifier',
+            'length' => 14,
+            'lengthType' => 'fixed',
+            'type' => 'number'
+            ),
+        '10' => array(
+            'name' => 'batch',
+            'length' => 20,
+            'lengthType' => 'variable',
+            'type' => 'string'
+            ),
+        '17' => array(
+            'name' => 'expirationDate',
+            'length' => 6,
+            'lengthType' => 'fixed',
+            'type' => 'date'
+        )
+    );
+    
+    protected $attributes = array();
+    
+    /**
+     * Returns key=>value array of attributes
+     */
+    public function parse($barcode) {
+        $this->attributes = array();
+        
+        for($index = 0; $index < strlen($barcode); $index) {
+            $identifier = substr($barcode, $index, 2);
+            if(!array_key_exists($identifier, $this->mapping)) {
+                return $this->attributes;
+            }
+            $index += 2+ $this->parseIdentifierSegment($this->mapping[$identifier], $index, $barcode);
+        }
+        return $this->attributes;
+    }
+    
+    
+    /**
+     * 
+     * @param array $segmentMapping
+     * @param unknown $startIndex
+     * @param unknown $barcodeString
+     * @return integer Number of characters read
+     */
+    protected function parseIdentifierSegment(array $segmentMapping, $startIndex, $barcodeString) {
+        if($segmentMapping['lengthType'] == 'fixed') {
+            $segmentLength = $segmentMapping['length'];
+        } else {
+            $endIndex = strstr(chr(29), substr($barcodeString, $startIndex));
+            if($endIndex === false)
+                $endIndex = strlen($barcodeString)-1;
+            $segmentLength = $endIndex-$startIndex;
+        }
+        
+        $segmentContents = substr($barcodeString, $startIndex+2, $segmentLength);
+        if($segmentMapping['type'] == 'date') {
+            $segmentContents = $this->convertToDate($segmentContents);
+        }
+        $this->attributes[$segmentMapping['name']] = $segmentContents;
+        return $segmentLength;
+    }
+    
+    protected function convertToDate($dateString) {
+        if(substr($dateString, 3, 2) == 00) {
+            $date = \DateTime::createFromFormat('ym01', substr($dateString, 0, 4));
+            return \DateTime::createFromFormat('Ymd', $date->format('Ymt'));
+        }
+        return \DateTime::createFromFormat('ymd', $dateString);
+    }
+}
+
+?>
