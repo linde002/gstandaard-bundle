8e6c3c168e49fbed55c4b2c81959e6e5f18fb0dc
Robert van der Linde <r.vanderlinde@pharma-intelligence.nl>
2015-08-01 14:50:19 +0200

    WMG & Schema update
    Signed-off-by: Robert van der Linde <r.vanderlinde@pharma-intelligence.nl>

--- Model/GsArtikelen.php
+++ Model/GsArtikelen.php
@@ -9,18 +9,22 @@ class GsArtikelen extends BaseGsArtikelen
 	const BTW_HOOG = '2';
 	const BTW_LAAG = '1';
 	const BTW_GEEN = '0';
-	
+
 	const MUTATIE_GEEN = 0;
 	const MUTATIE_VERWIJDER = 1;
 	const MUTATIE_WIJZIGEN = 2;
 	const MUTATIE_NIEUW = 3;
-	
+
 	const UN_UITVERKOOP = 'U';
 	const UN_VERVALLEN = 'V';
 	const UN_WETTELIJK_NIET_VERHANDELEN = 'W';
-	
+
 	const EAV_AFKORTING = 'EAV';
-	
+
+	public function isWmg() {
+	    return $this->getWtgkode() < 9;
+	}
+
 	public function getGtin() {
 		if(is_null($this->getLogistiekeInformatie())) {
 			return '';
@@ -28,46 +32,46 @@ class GsArtikelen extends BaseGsArtikelen
 			return $this->getLogistiekeInformatie()->getGtin();
 		}
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @return \PharmaIntelligence\GstandaardBundle\Model\GsThesauriTotaal
 	 */
 	public function getVerpakkingshoeveelheidEenheidOmschrijving() {
 	   if($this->getGsHandelsproducten()->getBasiseenheidOmschrijving()->getMemokodeItem() != 'ST')
 	       return $this->getDeelverpakkingOmschrijving();
-	   return $this->getGsHandelsproducten()->getBasiseenheidOmschrijving();    
+	   return $this->getGsHandelsproducten()->getBasiseenheidOmschrijving();
 	}
-	
+
 	public function isEAV() {
 		return $this->getDeelverpakkingOmschrijving()->getAfkorting() == self::EAV_AFKORTING;
 	}
-	
+
 	public function isRecordVervallen() {
 		return $this->getMutatiekode() == 1;
 	}
-	
+
 	public function isUitverkoop() {
 		return $this->getUnKode() == self::UN_UITVERKOOP;
 	}
-	
+
 	public function isUitverkocht() {
 		return $this->isUitverkoop() && $this->getUnDatum('U') < time();
 	}
-	
+
 	public function isVervallen() {
 		return $this->getUnKode() == self::UN_VERVALLEN;
 	}
-	
+
 	public function isActief() {
 		return $this->getMutatiekode() != self::MUTATIE_VERWIJDER;
 	}
-	
+
 	public function isWettelijkNietVerhandelen() {
 		return $this->getUnKode() == self::UN_WETTELIJK_NIET_VERHANDELEN;
 	}
-	
-	
+
+
 	public function getHeeftDeclaratieTitel() {
 		return GsDeclaratietabelDureGeneesmiddelenQuery::create('d')
 			->filterByZorgactiviteitVoldoetAanBeleidsregel(true)
@@ -77,7 +81,7 @@ class GsArtikelen extends BaseGsArtikelen
 			->where('d.Mutatiekode <> 1')
 			->count() > 0;
 	}
-	
+
 	public function getDeclaratieTitel() {
 		return GsDeclaratietabelDureGeneesmiddelenQuery::create('d')
 			->filterByZorgactiviteitVoldoetAanBeleidsregel(true)
@@ -87,7 +91,7 @@ class GsArtikelen extends BaseGsArtikelen
 			->where('d.Mutatiekode <> 1')
 			->findOne();
 	}
-	
+
 	protected function createRawSlug()
 	{
 		if(is_null($this->getGsNamen()))
@@ -95,11 +99,11 @@ class GsArtikelen extends BaseGsArtikelen
 		else
 			return '' . $this->cleanupSlugPart($this->getZinummer()) . '-' . $this->cleanupSlugPart($this->getGsNamen()->getNaamVolledig()) . '';
 	}
-	
+
 	public function getInkoopStuksPrijs() {
 		return $this->getInkoopprijs()/$this->getInkoophoeveelheid();
 	}
-	
+
 	public function getAantalBasisEenheden() {
 		return $this->getAantalHoofdverpakkingen()*$this->getAantalDeelverpakkingen()*$this->getHoeveelheidPerDeelverpakking();
 	}
