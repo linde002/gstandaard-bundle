b40a8d135102a0809a95ad9b2459b7990d38c18e
Robert van der Linde <r.vanderlinde@pharma-intelligence.nl>
2015-08-04 21:47:18 +0200

    Tekstmodule & RZV Bijlage 2
    Signed-off-by: Robert van der Linde <r.vanderlinde@pharma-intelligence.nl>

--- Model/GsTekstblokkenAsciiQuery.php
+++ Model/GsTekstblokkenAsciiQuery.php
@@ -6,4 +6,13 @@ use PharmaIntelligence\GstandaardBundle\Model\om\BaseGsTekstblokkenAsciiQuery;
 
 class GsTekstblokkenAsciiQuery extends BaseGsTekstblokkenAsciiQuery
 {
+    public static function getTekstVolledig($tekstModule, $tekstSoort, $tekstKode) {
+        return GsTekstblokkenHtmlQuery::create()
+            ->filterByTekstmodule($tekstModule)
+            ->filterByTekstsoort($tekstSoort)
+            ->filterByTekstNivoKode($tekstKode)
+            ->withColumn('GROUP_CONCAT(Tekst ORDER BY Tekstbloknummer, Tekstregelnummer SEPARATOR " ")', 'Tekst')
+            ->select('Tekst')
+            ->findOne();
+    }
 }
--- Model/GsTekstblokkenHtmlQuery.php
+++ Model/GsTekstblokkenHtmlQuery.php
@@ -6,4 +6,13 @@ use PharmaIntelligence\GstandaardBundle\Model\om\BaseGsTekstblokkenHtmlQuery;
 
 class GsTekstblokkenHtmlQuery extends BaseGsTekstblokkenHtmlQuery
 {
+    public static function getTekstVolledig($tekstModule, $tekstSoort, $tekstKode) {
+        return GsTekstblokkenHtmlQuery::create()
+        ->filterByTekstmodule($tekstModule)
+        ->filterByTekstsoort($tekstSoort)
+        ->filterByTekstNivoKode($tekstKode)
+        ->withColumn('GROUP_CONCAT(Tekst ORDER BY Tekstbloknummer, Tekstregelnummer SEPARATOR " ")', 'Tekst')
+        ->select('Tekst')
+        ->findOne();
+    }
 }
