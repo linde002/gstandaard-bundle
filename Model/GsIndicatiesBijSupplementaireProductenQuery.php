88ef8985a0505cc1bf7e0f2629fa7d3b540f184f
Robert van der Linde <r.vanderlinde@pharma-intelligence.nl>
2017-03-30 13:18:50 +0200

    BST007T
    Signed-off-by: Robert van der Linde <r.vanderlinde@pharma-intelligence.nl>

---
+++ Model/GsIndicatiesBijSupplementaireProductenQuery.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace PharmaIntelligence\GstandaardBundle\Model;
+
+use PharmaIntelligence\GstandaardBundle\Model\om\BaseGsIndicatiesBijSupplementaireProductenQuery;
+
+class GsIndicatiesBijSupplementaireProductenQuery extends BaseGsIndicatiesBijSupplementaireProductenQuery
+{
+}
